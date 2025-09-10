943df05c22c210c7566f6b788d2a6f6963745248
ryanttb <ryan@trippingthebits.com>
2025-09-10 11:42:27 -0400

    bullwinkle run animation

--- adv_rocky_bullwinkle/bullwinkle.gd
+++ adv_rocky_bullwinkle/bullwinkle.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -19,7 +20,15 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		# Flip sprite when moving left
+		animated_sprite.flip_h = direction < 0
+		# Play run animation when moving
+		if animated_sprite.animation != "run":
+			animated_sprite.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Play idle animation when not moving
+		if animated_sprite.animation != "idle":
+			animated_sprite.play("idle")
 
 	move_and_slide()
