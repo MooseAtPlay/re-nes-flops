6cf4dbd9212e541b4baf9903acc2491ca3610136
ryanttb <ryan@trippingthebits.com>
2025-10-16 18:27:18 -0400

    add first game and quit button

--- gallery.gd
+++ gallery.gd
@@ -5,18 +5,28 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Start the splash screen timer
-	await get_tree().create_timer(3.0).timeout
-	fade_out_intro_panel()
+	var timer = get_tree().create_timer(3.0)
+	timer.timeout.connect(_on_splash_timeout)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_splash_timeout() -> void:
+	fade_out_intro_panel()
 
 func fade_out_intro_panel():
 	if intro_panel:
 		# Create a tween for smooth fade-out
 		var tween = create_tween()
 		tween.tween_property(intro_panel, "modulate:a", 0.0, 1.0)
-		# Wait for fade to complete, then remove the node
-		await tween.finished
+		# Connect to tween completion signal instead of using await
+		tween.finished.connect(_on_fade_complete)
+
+func _on_fade_complete() -> void:
+	if intro_panel:
 		intro_panel.queue_free()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
